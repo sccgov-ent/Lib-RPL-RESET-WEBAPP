a04b49606228dab0587feb8c7d1cceef3796c9d1
Zachary Ian May <Ian.May@lib.sccgov.org>
2026-03-16 16:19:45 -0700

    added preparations for making backend provided over https.
    TODO: cert and key

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,15 @@
 const app = require('./app');
 const config = require('../config') || {};
+const https = require('https');
+const fs = require('fs');
 
 const port = process.env.PORT || config.port || 5000;
 
+var options = {
+  key: fs.readFileSync(process.env.SSLKEYPATH),
+  cert: fs.readFileSync(process.env.SSLCERTPATH)
+};
+
 // Start the server
-app.listen(port, () => {
-  console.log(`Backend listening on port ${port}`);
-});
+https.createServer(options, app).listen(443);
+console.log(`Backend listening on port 443`);
